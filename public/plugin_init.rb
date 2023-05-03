7b9e98541717b3c45a5d01c14c6f8c7b94110290
Kevin Clair <kevin@jackflaps.net>
2023-05-03 12:35:51 -0400

    revised v3.4.0 changes (includes Kaltura viewer support)

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,3 +1,8 @@
+require_relative 'helpers/viewer_helper'
+
 if !AppConfig.has_key?('enable_representative_file_version')
   AppConfig[:enable_representative_file_version] = false
 end
+
+AppConfig[:enable_mirador] = true
+AppConfig[:enable_kaltura] = true
